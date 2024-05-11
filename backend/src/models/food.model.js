1c95da83fa043b8f00968dd0ab7c8f8d17a21cdf
tuancamtbtx <nguyenvantuan140397@gmail.com>
2024-05-11 17:06:05 +0700

    [add] sequelize demo food

---
+++ backend/src/models/food.model.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, Sequelize) => {
+    const Foods = sequelize.define("foods", {
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      published: {
+        type: Sequelize.BOOLEAN
+      }
+    });
+  
+    return Foods;
+  };const Sequelize = require("sequelize");
+  const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+  
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
+  })
